e3b387298da94ca5f60b644e8c26b6c34abab9fe
Juliana <jcop@Jus-MacBook.local>
2019-11-20 12:47:05 -0300

    added file read & deriving policy key

--- nucypher_scripts/nucypher-testnet.py
+++ nucypher_scripts/nucypher-testnet.py
@@ -1,6 +1,7 @@
 from base64 import b64decode, b64encode                                 
 import json
 import requests
+
 gisalice = "http://localhost:8151"
 gisalice_keys = requests.get(f"{gisalice}/public_keys")
 print(gisalice_keys)
@@ -20,8 +21,6 @@ derived_label_data = json.loads(derived_label_json.content)
 
 prk = derived_label_data['result']['policy_encrypting_key']
 
-
-
 print(prk)
 
 # derived_response = requests.post(f"{gisalice}/derive_policy_encrypting_key/cholest_data_2019_10_08")
@@ -29,3 +28,13 @@ print(prk)
 
 
 # derived_label_json = requests.post(f"{gisalice}/derive_policy_encrypting_key/nameofthepdffile
+
+import file
+
+f = open("/Users/juliana/Documents/repos-hackathon/cypherbabies/lab_exams/Lab.Chol.2019.08.10.pdf", 'rb')
+data = f.read()
+
+
+print("Step 1: Deriving Policy Key")
+
+derived_label_json = requests.get(f"{gisalice}/derive_policy_encrypting_key/lab_chol_2019_08_10_pdf")
